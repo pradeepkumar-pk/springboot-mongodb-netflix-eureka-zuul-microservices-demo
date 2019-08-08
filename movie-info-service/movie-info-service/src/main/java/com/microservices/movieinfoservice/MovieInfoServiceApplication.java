14a6d723517af0b2f6dc3f66c71ddd143bbce006
Pradeep Kumar <pradeep.kumar@sysbiz.io>
2019-08-08 18:15:08 +0530

    modified for security implementation

--- movie-info-service/movie-info-service/src/main/java/com/microservices/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/movie-info-service/src/main/java/com/microservices/movieinfoservice/MovieInfoServiceApplication.java
@@ -3,6 +3,9 @@ package com.microservices.movieinfoservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,5 +14,23 @@ public class MovieInfoServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovieInfoServiceApplication.class, args);
 	}
+	
+
+
+@Configuration
+static class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+            .authorizeRequests().anyRequest().authenticated()
+                .and()
+            .oauth2ResourceServer().jwt();
+        // @formatter:on
+    }
+}
+
+
 
 }
--- ratings-data-service/ratings-data-service/src/main/java/com/microservices/moviedataservice/MovieDataServiceApplication.java
+++ ratings-data-service/ratings-data-service/src/main/java/com/microservices/moviedataservice/MovieDataServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -18,5 +21,23 @@ public class MovieDataServiceApplication {
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
+	
+
+
+@Configuration
+static class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+            .authorizeRequests().anyRequest().authenticated()
+                .and()
+            .oauth2ResourceServer().jwt();
+        // @formatter:on
+    }
+}
+
+
 
 }
